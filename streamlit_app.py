5f458ca4d0431e67d0092e9d70408757f9fe16cd
sjoonb <sungjun9808@gmail.com>
2024-09-20 23:53:36 +0900

    Replace langchain with llama-index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,51 @@
 import streamlit as st
-from langchain_openai.chat_models import ChatOpenAI
-
-st.title("🦜🔗 Quickstart App")
-
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+from llama_index.core import VectorStoreIndex, Settings, SimpleDirectoryReader
+from llama_index.llms.openai import OpenAI
+import openai
 
 
-def generate_response(input_text):
-    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
-    st.info(model.invoke(input_text))
+# Initialize message history
+openai.api_key = st.secrets.openai_key
+st.header("Chat with the Streamlit docs 💬 📚")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+    ]
 
 
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
-    )
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
-    if submitted and openai_api_key.startswith("sk-"):
-        generate_response(text)
+Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
+
+# Load and index data
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        index = VectorStoreIndex.from_documents(docs)
+        return index
+
+
+index = load_data()
+
+# Create the chat engine
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+# Prompt for user input and display message history
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+# Pass query to chat engine and display response
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
